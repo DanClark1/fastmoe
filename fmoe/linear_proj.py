9ac8b32e5c2ad71879df61426d62627d179fdaac
DanClark1 <44947380+DanClark1@users.noreply.github.com>
2025-03-22 17:59:18 +0000

    debug

--- fmoe/linear_proj.py
+++ fmoe/linear_proj.py
@@ -75,6 +75,8 @@ class FMoELinearProj(nn.Module):
         r"""
         Call MOE function
         """
+
+        print(inp, fwd_expert_count)
         x = MOELinear.apply(inp, fwd_expert_count, self.weight, self.bias)
 
         counts = fwd_expert_count if isinstance(fwd_expert_count, torch.Tensor) else \
